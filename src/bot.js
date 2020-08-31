01b4cc34f2c7be7d7b69074be2b07a31a75386bb
Vicky Wang <vickywang@Vickys-MacBook-Pro.local>
2020-08-31 18:12:49 -0400

    add project

---
+++ src/bot.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+
+const { Client, WebhookClient } = require('discord.js');
+
+const client = new Client({
+    partials: ['MESSAGE', 'REACTION']
+});
+
+const webhookClient = new WebhookClient(
+    process.env.WEBHOOK_ID,
+    process.env.WEBHOOK_TOKEN
+);
+
+const PREFIX = "$";
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`)
+})
+
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+
+        if (CMD_NAME === 'kick') {
+            if (!message.member.hasPermission('KICK_MEMBERS'))
+                return message.reply('You do not have permissions to use that command');
+
+            if (args.length === 0) return message.reply('Please provide an ID');
+            const member = message.guild.members.cache.get(args[0]);
+
+            if (member) {
+                member.kick()
+                    .then((member) => message.channel.send(`${member} was kicked.`))
+                    .catch((err) => message.channel.send('I cannot kick that user :('))
+            } else {
+                message.channel.send('That member was not found');
+            }
+        } else if (CMD_NAME === 'ban') {
+            if (!message.member.hasPermission('BAN_MEMBERS'))
+                return message.reply('You do not have permissions to use that command');
+            if (args.length === 0) return message.reply("Please provide an ID");
+
+            try {
+                const user = await message.guild.members.ban(args[0]);
+                message.channel.send('User was banned successfully');
+            } catch (err) {
+                console.log(err)
+                message.channel.send('An error occurred. Either I do not have permissions or the user was not found');
+            }
+        } else if (CMD_NAME === 'announce') {
+            console.log(args);
+            const msg = args.join(' ');
+            console.log(msg);
+            webhookClient.send(msg)
+        }
+    }
+})
+
+// client.on('messageReactionAdd', (reaction, user) => {
+//     const { name } = reaction.emoji;
+//     const member = reaction.message.guild.members.cache.get(user.id);
+//     if (reaction.message.id === '750102121722151022') {
+//         switch (name) {
+//             case '🍎':
+//                 console.log(name)
+//                 member.roles.add('750101611535401011');
+//                 break;
+//             case '🍌':
+//                 console.log(name)
+//                 // member.roles.add('750101716111982723');
+//                 break;
+//             case '🍇':
+//                 console.log(name)
+//                 // member.roles.add('750101685208219649');
+//                 break;
+//             case '🍑':
+//                 console.log(name)
+//                 // member.roles.add('750101664559530014');
+//                 break;
+//         }
+//     }
+// })
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
